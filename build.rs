3fad7d5ac2de4bc894c440f505196d19b1d33b60
Ayrzo <ayrzo@lurito.cn>
2025-03-15 07:25:13 +0800

    fix (build): 解决 docs.rs 不能构建文档的问题

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    #[cfg(not(target_os = "windows"))]
-    panic!("This library only supports Windows.");
+    // Check if the target OS is Windows, but allow docs.rs for building docs
+    if !cfg!(target_os = "windows") && std::env::var("DOCS_RS").is_err() {
+        panic!("This library only supports Windows.");
+    }
 }
